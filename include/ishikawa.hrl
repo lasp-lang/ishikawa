d64826fad3b2d87a0172c63ea54d3764470910a1
Christopher Meiklejohn <christopher.meiklejohn@gmail.com>
2016-10-15 19:38:22 +0100

    Add additional configuration.

--- include/ishikawa.hrl
+++ include/ishikawa.hrl
@@ -1,4 +1,8 @@
+-define(PEER_IP, {127,0,0,1}).
+-define(PEER_PORT, 9000).
+
 -type actor() :: binary().
 -type message() :: binary().
 -type timestamp() :: vclock:vclock().
 -type timestamp_matrix() :: mclock:mclock().
+-type node_spec() :: {node(), inet:ip_address(), non_neg_integer()}.
